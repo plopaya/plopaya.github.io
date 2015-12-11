778d24412ec88a78d0eb7acec8a816677b8aa299
plopaya <lilwizz509@gmail.com>
2015-12-10 16:33:03 -0800

    updates to my new and improved page

---
+++ js/b-script.js
@@ -0,0 +1,18 @@
+// my code
+$('article').readmore({
+  embedCSS: false, 
+  moreLink: '<article><a href="#">Show</a></article>',
+  lessLink: '<article><a href="#">Hide</a></article>',
+  speed: 500
+  
+});
+
+// Class example...Teacher code of the Revealer
+// $('article').readmore({
+
+// embedCSS : false,
+// moreLink: '<article><a href="#">Reveal Thy Self</a></article>', 
+// lessLink: '<article><a href="#">Dim Hither</a></article>',
+// speed: 1000
+
+// });
---
+++ js/p-script.js
@@ -0,0 +1,14 @@
+// my change to the code 
+$(document).ready(function() {
+ 
+  $('.images a').magnificPopup({type:'image'});
+
+});
+
+// Class example...Teacher code of the lightBox
+
+//$(document).ready(function() {
+ 
+ // $('a').magnificPopup({type:'image'});
+
+  //  });
